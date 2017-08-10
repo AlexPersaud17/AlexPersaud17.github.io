bacc6b0eb553accaa42b0f2ec95a62837eee6fd4
Alex Persaud <alex_persaud01@yahoo.com>
2017-08-09 20:25:25 -0400

    Add logo

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import Logo from './images/logo.png'
 
 class NavBar extends Component {
   render(){
     return(
       <div id='nav-bar'>
+        
         <header>
+          <div>
+            <a href="#landing"><img id='nav-logo' src={Logo}/></a>
+          </div>
           <nav>
             <a className="tabs" href="#contact-text">Contact Me!</a>
             <a className="tabs" href="#resume-text">Résumé</a>
